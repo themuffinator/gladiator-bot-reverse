1614c66c91fe16cb30688a83d04d729ed63e09f8
themuffinator <djdacosta86@yahoo.co.uk>
2025-11-05 20:58:45 +0000

    Stub botlib dependencies for precompiler tests

--- tests/chat/test_bot_chat_stubs.c
+++ tests/chat/test_bot_chat_stubs.c
@@ -1,7 +1,9 @@
+#include "botlib/common/l_assets.h"
 #include "botlib/common/l_log.h"
 #include "botlib/common/l_memory.h"
 #include "botlib/interface/botlib_interface.h"
 
+#include <stdbool.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -32,10 +34,41 @@ float LibVarValue(const char *var_name, const char *default_value) {
     return 0.0f;
 }
 
+void *GetMemory(size_t size) {
+    return malloc(size);
+}
+
 void *GetClearedMemory(size_t size) {
     return calloc(1, size);
 }
 
 void FreeMemory(void *ptr) {
     free(ptr);
+}
+
+void BotLib_LogWrite(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+    fputc('\n', stderr);
+}
+
+bool BotLib_LocateAssetRoot(char *buffer, size_t size) {
+    if (buffer == NULL || size == 0) {
+        return false;
+    }
+
+    if (BOT_ASSET_ROOT[0] == '\0') {
+        buffer[0] = '\0';
+        return false;
+    }
+
+    int written = snprintf(buffer, size, "%s", BOT_ASSET_ROOT);
+    if (written < 0 || (size_t)written >= size) {
+        buffer[0] = '\0';
+        return false;
+    }
+
+    return true;
 }
