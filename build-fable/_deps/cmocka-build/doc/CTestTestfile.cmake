81e278d2a613f0628b6b0281da8b1125aa77f7fa
Prof. Muffi Nator PhD <djdacosta86@yahoo.co.uk>
2026-07-15 22:58:11 +0100

    Delete build-fable directory

--- build-fable/_deps/cmocka-build/doc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Repositories/Gladiator-Bot-reverse/build-fable/_deps/cmocka-src/doc
-# Build directory: E:/Repositories/Gladiator-Bot-reverse/build-fable/_deps/cmocka-build/doc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build-fable/_deps/cmocka-build/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Repositories/Gladiator-Bot-reverse/build-fable/_deps/cmocka-src/include
-# Build directory: E:/Repositories/Gladiator-Bot-reverse/build-fable/_deps/cmocka-build/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
