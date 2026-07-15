57e20f8b747a4191ec6937a808f441b8f47b5467
Prof. Muffi Nator PhD <djdacosta86@yahoo.co.uk>
2026-07-15 22:58:21 +0100

    Delete build-release-codex directory

--- build-release-codex/_deps/cmocka-build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Repositories/Gladiator-Bot-reverse/build-release-codex/_deps/cmocka-src
-# Build directory: E:/Repositories/Gladiator-Bot-reverse/build-release-codex/_deps/cmocka-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("include")
-subdirs("src")
-subdirs("doc")
--- build-release-codex/tests/chat/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Repositories/Gladiator-Bot-reverse/tests/chat
-# Build directory: E:/Repositories/Gladiator-Bot-reverse/build-release-codex/tests/chat
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(bot_chat "E:/Repositories/Gladiator-Bot-reverse/build-release-codex/tests/chat/bot_chat_tests.exe")
-set_tests_properties(bot_chat PROPERTIES  _BACKTRACE_TRIPLES "E:/Repositories/Gladiator-Bot-reverse/tests/chat/CMakeLists.txt;32;add_test;E:/Repositories/Gladiator-Bot-reverse/tests/chat/CMakeLists.txt;0;")
